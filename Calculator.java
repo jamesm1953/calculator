7a0d1fc63051569fbc9a7b9a3a96e73b15264135
collaborator <james.martin@usablesystems.com>
2023-10-04 17:52:04 -0500

    Added multiply methods

--- Calculator.java
+++ Calculator.java
@@ -7,5 +7,13 @@ public class Calculator {
 		return a + b;
 	}
 
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+
 	public static void main(String[] args) {}
 }
